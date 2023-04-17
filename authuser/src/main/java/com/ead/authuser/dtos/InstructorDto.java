3b801a99360ff144fc2850e048453aa25da6cf34
ccleao <98561136+ccleao@users.noreply.github.com>
2023-04-17 08:35:24 -0300

    AUTHUSER Sychronous Communication Version

---
+++ authuser/src/main/java/com/ead/authuser/dtos/InstructorDto.java
@@ -0,0 +1,12 @@
+package com.ead.authuser.dtos;
+
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class InstructorDto {
+
+    private UUID userId;
+}
---
+++ authuser/src/main/java/com/ead/authuser/models/UserCourseModel.java
@@ -0,0 +1,33 @@
+package com.ead.authuser.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_USERS_COURSES")
+public class UserCourseModel extends RepresentationModel<UserModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private UserModel user;
+
+    @Column(nullable = false)
+    private UUID courseId;
+
+
+}
